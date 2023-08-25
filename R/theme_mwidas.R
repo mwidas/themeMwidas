664109237794f8056fd9fe3d1cc0fd34b7676b63
mwidas <mel.widas@gmail.com>
2023-08-25 11:05:28 -0700

    update

--- R/theme_mwidas.R
+++ R/theme_mwidas.R
@@ -5,7 +5,8 @@
 #'
 #' @examples
 theme_mwidas <- function() {
-  theme(plot.background = element_rect(fill = "magenta"),
-        panel.background = element_rect(fill = "gray"),
-        panel.grid = element_line(color = "cyan"))
+  theme(plot.background = element_rect(fill = "white"),
+        panel.background = element_rect(fill = "lightgrey"),
+        panel.grid = element_line(color = "white"),
+        title = element_text(size = 12, color = "pink"))
 }
